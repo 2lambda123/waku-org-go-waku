2bc67342662a4f8e43dd6d9125d6ff22e3fac874
Restyled.io <commits@restyled.io>
2024-08-15 16:23:48 +0000

    Restyled by shellcheck

--- scripts/linux/fpm-build.sh
+++ scripts/linux/fpm-build.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )/../../
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit ; pwd -P )/../../
 
-VERSION=`cat ${parent_path}/VERSION`
+VERSION=$(cat "${parent_path}"/VERSION)
 
-if [ ! -f ${parent_path}/build/waku ]
+if [ ! -f "${parent_path}"/build/waku ]
 then
     echo "waku binary does not exist. Execute make first"
     exit
 fi
 
-tmpdir=`mktemp -d`
+tmpdir=$(mktemp -d)
 
-chmod 777 ${tmpdir}
+chmod 777 "${tmpdir}"
 
-cp ${parent_path}/build/waku ${tmpdir}
+cp "${parent_path}"/build/waku "${tmpdir}"
 
-strip --strip-unneeded ${tmpdir}/waku
+strip --strip-unneeded "${tmpdir}"/waku
 
-cp ${parent_path}/scripts/linux/waku.service ${tmpdir}
+cp "${parent_path}"/scripts/linux/waku.service "${tmpdir}"
 
-pushd ${tmpdir}
+pushd "${tmpdir}" || exit
 
 fpm \
   -s dir -t deb \
-  -p gowaku-${VERSION}-x86_64.deb \
+  -p gowaku-"${VERSION}"-x86_64.deb \
   --name go-waku \
   --license "MIT, Apache 2.0" \
-  --version ${VERSION} \
+  --version "${VERSION}" \
   --architecture x86_64 \
   --depends libc6 \
   --description "Go implementation of Waku v2 protocol" \
@@ -36,10 +36,10 @@ fpm \
   --deb-systemd=waku.service \
   waku=/usr/bin/waku
 
-fpm -s deb -t rpm -p gowaku-${VERSION}-x86_64.rpm *.deb
+fpm -s deb -t rpm -p gowaku-"${VERSION}"-x86_64.rpm *.deb
 
-mv *.deb *.rpm ${parent_path}/build/.
+mv *.deb *.rpm "${parent_path}"/build/.
 
-ls ${parent_path}/build/.
+ls "${parent_path}"/build/.
 
-popd
+popd || exit
